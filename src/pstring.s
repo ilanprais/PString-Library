c2a5344d659e401993c703c829b3418727a6f269
Ilan Prais <ilandovprais@gmail.com>
2020-12-15 12:33:33 +0200

    replace char finished

--- src/pstring.s
+++ src/pstring.s
@@ -1,25 +1,27 @@
 .section	.rodata	#read only data section
-str:	.string	"%d\n"
+str:	.string	"ere\n"
     
     .text
 .globl pstrlen
     .type pstrlen, @function
-
 pstrlen:
     movzbq (%rdi),%rax
     ret
 
+.globl replaceChar
+    .type replaceChar, @function
 replaceChar:
-    mov $-1,%rcx
+    movzbq (%rdi),%r11
+    mov $0,%r10
     .L1:
-        addq $1, %rcx
-        cmpq (%rdi,%rcx,8),%rsi
+        addq $1, %r10
+        movzbq (%rdi,%r10,1),%rbx
+        cmpq %rbx,%rsi
         jne .L2
-        mov %rdx,(%rdi,%rcx,8)
+        movb %dl,(%rdi,%r10,1)
     .L2:
-        cmpq (%rdi),%rcx
-        jge .L1
-    
+        cmpq %r11,%r10
+        jle .L1
     ret
 
 pstrijcpy:
