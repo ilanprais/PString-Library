e346370ad678866edb0f2b58808662bbfb41b75f
Ilan Prais <ilandovprais@gmail.com>
2020-12-15 17:02:02 +0200

    working on strijcmp

--- src/pstring.s
+++ src/pstring.s
@@ -96,5 +96,45 @@ swapCase:
         movb %bl,(%rdi,%r10,1)
         jmp .L7
 
-
+.globl pstrijcmp
+    .type pstrijcmp, @function
 pstrijcmp:
+    # out of bounds check
+    movzbq (%rdi),%r10
+    movzbq (%rsi),%r11
+    cmp $0,%rdx # check i < 0
+    jl .L17
+    cmp %r10,%rcx # check j >= len
+    jge .L17
+    cmp %r11,%rcx
+    jge .L17
+    cmp %r10,%r11 # check i > j
+    jl .L17
+
+    movq %rdx,%r10 # i counter
+    movq %rcx,%r11 # j counter
+    .L14:
+        cmp %r11,%r10
+        jle .L16
+    .L15:
+        movq $0, %rax 
+        ret
+    .L18:
+        movq $1, %rax
+        ret
+    .L19:
+        movq $-1, %rax
+        ret
+    .L16:
+        addq $1,%r10
+        movb (%rsi,%r10,1), %bl
+        cmp %bl,(%rdi,%r10,1)
+        jg .L18
+        jl .L19
+        jmp .L14
+    .L17:
+        movq $-2, %rax 
+        movq $invalid,%rdi  
+        movq $0,%rax
+        call printf
+        ret
