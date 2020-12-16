5b130a54f88039d8a924135aee0fb84ca895019b
Ilan Prais <ilandovprais@gmail.com>
2020-12-16 21:31:46 +0200

    format

--- src/pstring.s
+++ src/pstring.s
@@ -1,30 +1,34 @@
 .section	.rodata	#read only data section
 invalid:    .string "invalid input!\n"
+# The main code section #
     .text
-    
+
+# Pstring length #
 .globl pstrlen
     .type pstrlen, @function
 pstrlen:
-    movzbq  (%rdi),%rax
+    movzbq  (%rdi),%rax # Return first byte of pstring
     ret
 
+# Replace old char for new char #
 .globl replaceChar
     .type replaceChar, @function
 replaceChar:
-    movzbq  (%rdi),%r11
-    mov     $0,%r10
-    .L1:
-    addq    $1, %r10
-    movzbq  (%rdi,%r10,1),%rbx
-    cmpq    %rbx,%rsi
+    movzbq  (%rdi),%r11 # Save string length at %r11
+    mov     $0,%r10 # Loop counter
+    .L1: # main loop body
+    addq    $1, %r10 # increase counter
+    movzbq  (%rdi,%r10,1),%rbx # check char at current place
+    cmpq    %rbx,%rsi # compare current char with old char
     jne     .L2
-    movb    %dl,(%rdi,%r10,1)
-    .L2:
+    movb    %dl,(%rdi,%r10,1) # if equal then replace with new
+    .L2: # loop termination check
     cmpq    %r11,%r10
-    jle     .L1
-    movq    %rdi, %rax
+    jle     .L1 # if not reached end then return to beginning 
+    movq    %rdi, %rax # loop finished
     ret
 
+# Copy str1[i:j] to str2[i:j] #
 .globl pstrijcpy
     .type pstrijcpy, @function
 pstrijcpy:
@@ -42,18 +46,19 @@ pstrijcpy:
 
     movq    %rdx,%r10 # i counter
     movq    %rcx,%r11 # j counter
-    .L3:
+    # loop
+    .L3: # loop termination check (check if i reached j)
     cmp     %r11,%r10
-    jle     .L5
-    .L4:
+    jle     .L5 # if not then move to main body
+    .L4: # termination and return
     movq    %rdi, %rax
     ret
-    .L5:
-    addq    $1,%r10
+    .L5: # main loop body
+    addq    $1,%r10 # increase i by 1
     movb    (%rsi,%r10,1), %bl
-    movb    %bl,(%rdi,%r10,1)
-    jmp     .L3
-    .L6:
+    movb    %bl,(%rdi,%r10,1) # move str1[i] to str2[i]
+    jmp     .L3 # return to loop termination check
+    .L6: # invalid message and termination (from bound check)
     movq    %rdi, %rbx
     movq    $invalid,%rdi  
     movq    $0,%rax
@@ -62,41 +67,44 @@ pstrijcpy:
     movq    %rbx, %rax
     ret
 
+# Swap letter case #
 .globl swapCase
     .type swapCase, @function
 swapCase:
-    movzbq  (%rdi),%r11
-    mov     $0,%r10
-    .L7:
+    movzbq  (%rdi),%r11 # save length
+    mov     $0,%r10 # loop counter
+    # loop
+    .L7: # loop termination check (check if i reached length)
     cmp     %r11,%r10
     jle     .L9
-    .L8:
+    .L8: # termination and return
     ret
-    .L9:
-    addq    $1,%r10
-    movb    (%rdi,%r10,1), %bl
-    cmp     $97, %bl
+    .L9: # main loop body
+    addq    $1,%r10 # increase counter
+    movb    (%rdi,%r10,1), %bl # save current char
+    cmp     $97, %bl # lower case lower bound check
     jge     .L10
-    cmp     $65, %bl
+    cmp     $65, %bl # heigher case lower bound check
     jge     .L11
     jmp     .L7
-    .L10:
-    cmp     $122, %bl
+    .L10: # lower case heigher bound check
+    cmp     $122, %bl 
     jle     .L12
     jmp     .L7
-    .L11:
+    .L11: # heigher case heigher bound check
     cmp     $90, %bl
     jle     .L13
     jmp     .L7
-    .L12:
+    .L12: # lower case to heigher swap
     subb    $32,%bl
     movb    %bl,(%rdi,%r10,1)
     jmp     .L7
-    .L13:
+    .L13: # heigher case to lower swap
     addb    $32,%bl
     movb    %bl,(%rdi,%r10,1)
     jmp     .L7
 
+# Compare str1[i:j] and str2[i:j] #
 .globl pstrijcmp
     .type pstrijcmp, @function
 pstrijcmp:
